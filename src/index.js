36c140f06974fec9bf9a8646537befe7b61224c4
dianakay <dianakay333@gmail.com>
2018-09-23 22:12:42 +0300

    sort

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class Sorter {
-  constructor() {
-    // your implementation
+  constructor(arr=[]) {
+    this.arr=arr;
   }
 
   add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
+    this.arr.push(element);
+	return this.arr;
   }
 
   get length() {
-    // your implementation
+	  
+	  return this.arr.length;
   }
 
+  at(index) {
+	  return this.arr[index];
+  }
+  
   toArray() {
-    // your implementation
+	var mass=[];
+    for (var i=0; i<this.arr.length; i++) {
+		mass[i]=this.arr[i];
+	}
+	return mass;
   }
 
   sort(indices) {
-    // your implementation
-  }
+	 var n = indices.length;
+    for (var i = 0; i < n; i++) {
+		for (var i = 0; i < indices.length; i++){
+		var v = this.arr[indices[ i ]], j = i-1;
+		while (j >= 0 && this.arr[indices[ j ]] > v)
+				{ this.arr[indices[ j+1 ]] = this.arr[indices[ j ]]; j--; }
+		this.arr[indices[ j+1 ]] = v;
+     }    
+	 }	 
+    return this.arr; 
+}
 
   setComparator(compareFunction) {
     // your implementation
